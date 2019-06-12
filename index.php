a2639f8f36d9c6c71a25f49bcfd238ee67c169f7
rozziqputra <rozziqputra@gmail.com>
2019-06-12 20:47:53 +0800

    upload 1

---
+++ index.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+  <!-- my css -->
+  <link rel="stylesheet" href="style.css">
+  <!-- my font -->
+  <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark" >
+    <div class="container">
+      <a class="navbar-brand" href="#">Leo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Features</a>
+          <a class="nav-item nav-link" href="#">Pricing</a>
+          <a class="nav-item nav-link " href="#">about</a>
+          <a class="nav-item btn btn-primary tombol" href="#">join us</a>
+
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- ahir navbar -->
+
+  <!-- jumbo tron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Porto<span>Folio by <br></span>@<span>rozziqputra</span>.com</h1>
+      <a href="" class="btn btn-primary tombol">our work</a>
+    </div>
+  </div>
+  <!-- ahir tron -->
+
+  <!-- container -->
+
+  <div class="container">
+    <!-- info panel -->
+
+    <div class="row justify-content-center">
+      <div class="col-10 info-panel">
+        <div class="row">
+          <div class="col-lg">
+            <img src="img/img/employee.png" alt="employe" class="float-left">
+            <h4>24 Hours</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div class="col-lg">
+            <img src="img/img/hires.png" alt="haires" class="float-left">
+            <h4>haires</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div class="col-lg">
+            <img src="img/img/security.png" alt="employe" class="float-left">
+            <h4>sekurity</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ahir info panel -->
+    <!-- working space -->
+    <div class="row workingspace">
+      <div class="col-lg-6">
+        <img src="img/img/workingspace.png" alt="workingspace" class="img-fluid">
+      </div>
+      <div class="col-lg-5">
+        <h3> You <span>work </span>like at <span>Home</span></h3>
+        <p>Bekerja dengan seuasana hati yang lebih asik dan mempelajari hal baru setipa harinya</p>
+        <a href="" class="btn btn-primary tombol">Galery</a>
+      </div>
+    </div>
+    <!--akhir working space -->
+
+    <!-- testimonial -->
+    <section class="testimonial">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 d-flex">
+          <h5>"Yang mau carik jalan yang gak mau Carik alasan, Orang bisa Karna terbiasa"</h5>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-6 d-flex justify-content-center">
+          <figure class="figure">
+            <img src="img/img/img1.png" class="figure-img img-fluid rounded-circle" alt="figure 1">
+          </figure>
+          <figure class="figure">
+            <img src="img/img/img2.png" class="figure-img img-fluid rounded-circle utama" alt="figure 2">
+            <figcaption class="figure-caption">
+              <h5>Leny Khurana</h5>
+              <p>Design</p>
+            </figcaption>
+          </figure>
+          <figure class="figure">
+            <img src="img/img/img3.png" class="figure-img img-fluid rounded-circle" alt="figure 3">
+          </figure>
+        </div>
+      </div>
+    </section>
+    <!-- ahir testimonial -->
+
+   <div class="row footer">
+     <div class="col text-center">
+        <p>2018 All Rights Reserved by rozziqputra </p>
+      </div>
+   </div>
+    <!-- ahir footer -->
+
+
+
+
+  </div>
+
+
+  <!-- ahir container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
